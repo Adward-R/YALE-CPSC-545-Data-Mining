5039b1776023a9f9fe70086b1b8c82869c337692
Eddie <adward_r@icloud.com>
2016-11-18 02:24:14 -0500

    hw04 - SVM - P3

--- 04_svm_clustering/script3.m
+++ 04_svm_clustering/script3.m
@@ -4,24 +4,53 @@ function script3
     data = load('simple_iris.mat');
     x = data.x;
     c = data.c;
-    kh = @(x,y) x*y';
-    Md = svm_train(c,x,kh);
-    [chat, d] = svm_classify(Md, x);
+    kh = @(x, y) x * y';
+    Md = svm_train(c, x, kh);
+    % [chat, d] = svm_classify(Md, x);
 
     figure;
     hold all;
     plot(x(c==1, 1), x(c==1, 2), 'bo');
     plot(x(c==-1, 1), x(c==-1, 2), 'ro');
-    scatter(x(abs(Md.v)>0.001, 1), x(abs(Md.v)>0.001, 2), 50, 'k', 'filled');
+    sup_vec_ind = abs(Md.v) > 0.001;
+    scatter(x(sup_vec_ind, 1), x(sup_vec_ind, 2), 50, 'k', 'filled');
+    w = x' * Md.v;  % col vec
+    for width = [-1, 0, 1],
+        f = @(t) (Md.b + width - w(1) * t) / w(2);
+        ezplot(f, [min(x(:, 1)), max(x(:, 1))]);
+    end
+    title('Linear SVM');
+    print('3-1', '-dpng');
     hold off;
+    close all;
 
     % Quadratic kernel SVM testing data
     data = load('simple_nonlinear.mat');
-    % x = data.x;
-    % c = data.c;
-    % kh = []; % implement quadratic kernel
-    % Md = svm_train(c,x,kh);
+    x = data.x;
+    c = data.c;
+    kh = @(x, y) (x * y' + 1)^2 ; % implement quadratic kernel
+    Md = svm_train(c, x, kh);
+    % Create a 20x20 grid that covers the training set
+    coors_x = linspace(min(x(:, 1)), max(x(:, 1)), 20);
+    coors_y = linspace(min(x(:, 2)), max(x(:, 2)), 20);
+    [X, Y] = meshgrid(coors_x, coors_y);
+    y = [reshape(X, [20^2 1]) reshape(Y, [20^2 1])];
+    [chat, d] = svm_classify(Md, y);
 
+    figure;
+    hold all;
+    plot(y(chat==1, 1), y(chat==1, 2), 'bo');
+    plot(y(chat==-1, 1), y(chat==-1, 2), 'ro');
+    sup_vec_ind = abs(Md.v) > 0.001;
+    scatter(y(sup_vec_ind, 1), y(sup_vec_ind, 2), 50, 'k', 'filled');
+    title('Quadratic Kernel SVM 1');
+    print('3-2', '-dpng');
+    hold off;
+
+    scatter(y(:, 1), y(:, 2), 30, d);
+    title('Quadratic Kernel SVM 2');
+    print('3-3', '-dpng');
+    close all;
 return
 end
 
@@ -48,7 +77,7 @@ function Md = svm_train(c,x,kh)
     bias = 0;
     non_zeros = 0;
     for j = 1:N,
-        if lambda(j) > 0,
+        if abs(lambda(j)) > 0.001,
             non_zeros = non_zeros + 1;
             bias = bias - c(j);
             for i = 1:N,
